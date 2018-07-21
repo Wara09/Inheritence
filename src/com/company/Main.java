390e16188e70b450ccefdaee7215a31603f88123
wara09 <tahmidawara01@gmail.com>
2018-07-21 14:41:02 +0600

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       Cat white=new Cat();
+       Cat black=new Cat();
+       Cat brown=new Cat();
+
+        System.out.println("White Cat has "+ white.numberOfEyes +" Eyes, "+ white.numberOfEars+" Ears, " + white.numberOfTail+" Tail");
+        System.out.println("Black Cat has "+ black.numberOfEyes +" Eyes, "+ black.numberOfEars+" Ears, " + black.numberOfTail+" Tail");
+        System.out.println("Brown Cat has "+ brown.numberOfEyes +" Eyes, "+ brown.numberOfEars+" Ears, " + brown.numberOfTail+" Tail");
+
+
+
+
+    }
+}
